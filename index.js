a04cf529e927ee46541fae156a5a1ba7b22b0af3
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-06 10:36:38 +0300

    fix for empty input

--- index.js
+++ index.js
@@ -11,26 +11,29 @@ app.get('/', (req, res) => {
 
 app.post('/analyze', (req, res) => {
     const inputText = req.body.text;
-    const totalLength = inputText.length;
-    const lengthWithoutSpace = inputText.replace(/ /g, '').length;
-    const wordCount = inputText.match(/(\w+)/g).length;
-    console.log(inputText);
-    console.log(totalLength);
-    console.log(lengthWithoutSpace);
-    console.log(wordCount);
+    let totalLength = 0;
+    let lengthWithoutSpace = 0;
+    let wordCount = 0;
+    let result = [];
+    if (inputText !== '') {
+        totalLength = inputText.length;
+        lengthWithoutSpace = inputText.replace(/ /g, '').length;
+        wordCount = inputText.match(/(\w+)/g).length;
 
-    let counter = (str) => {
-        return str
-            .split('')
-            .filter((char) => /[a-zA-Z]/.test(char))
-            .reduce((total, char) => {
-                total[char] ? total[char]++ : (total[char] = 1);
-                return total;
-            }, {});
-    };
-    const out = counter(inputText.toLowerCase());
-    const ordered = Object.fromEntries(Object.entries(out).sort());
-    const result = Object.keys(ordered).map((key) => ({ [key]: ordered[key] }));
+        let counter = (str) => {
+            return str
+                .split('')
+                .filter((char) => /[a-zA-Z]/.test(char))
+                .reduce((total, char) => {
+                    total[char] ? total[char]++ : (total[char] = 1);
+                    return total;
+                }, {});
+        };
+        const out = counter(inputText.toLowerCase());
+        const ordered = Object.fromEntries(Object.entries(out).sort());
+        result = Object.keys(ordered).map((key) => ({ [key]: ordered[key] }));
+    }
+
     const responseObject = {
         textLength: { withSpaces: totalLength, withoutSpaces: lengthWithoutSpace },
         wordCount: wordCount,
