901a72bfaece54ee7f5fb35212ca0cd251e0206f
Lalit Ghimire <lalitghimire@gmail.com>
2022-05-14 17:02:37 +0300

    refactor code

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello to dreambroker assignment' });
-});
 
 app.post('/analyze', (req, res) => {
     const inputText = req.body.text;
     let totalLength = 0;
     let lengthWithoutSpace = 0;
     let wordCount = 0;
-    let result = [];
+    let charCountArray = [];
+
     if (inputText !== '') {
         totalLength = inputText.length;
         lengthWithoutSpace = inputText.replace(/ /g, '').length;
         wordCount = inputText.match(/(\w+)/g).length;
 
-        let counter = (str) => {
+        // function which creates an object with characters and counts from a string
+        const characterCounter = (str) => {
             return str
                 .split('')
                 .filter((char) => /[a-zA-Z]/.test(char))
@@ -29,15 +28,21 @@ app.post('/analyze', (req, res) => {
                     return total;
                 }, {});
         };
-        const out = counter(inputText.toLowerCase());
-        const ordered = Object.fromEntries(Object.entries(out).sort());
-        result = Object.keys(ordered).map((key) => ({ [key]: ordered[key] }));
+
+        const characterCountObject = characterCounter(inputText.toLowerCase());
+        const orderedCharacterCountObject = Object.fromEntries(
+            Object.entries(characterCountObject).sort()
+        );
+        // create an array of the objects from a single object and save to result
+        charCountArray = Object.keys(orderedCharacterCountObject).map((key) => ({
+            [key]: orderedCharacterCountObject[key],
+        }));
     }
 
     const responseObject = {
         textLength: { withSpaces: totalLength, withoutSpaces: lengthWithoutSpace },
         wordCount: wordCount,
-        characterCount: result,
+        characterCount: charCountArray,
     };
     res.send(responseObject);
 });
